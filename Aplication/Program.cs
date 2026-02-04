fd33a5d8c91cc98d2c035ddd59e1895a254b9003
dror <dror@AVIV-LENOVO>
2026-02-04 08:20:54 +0200

    Add project files.

---
+++ Aplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication 
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
