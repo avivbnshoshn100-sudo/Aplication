1ebc42040465588dea117a924e8d40532633eb1f
avivbn.shoshn100@gmail.com <avivbn.shoshn100@gmail.com>
2026-02-04 08:45:01 +0200

    secend commit

--- Aplication/Program.cs
+++ Aplication/Program.cs
@@ -12,9 +12,21 @@ namespace Aplication
     {
         static void Main(string[] args)
         {
+
+
+
+            //Test change number 2
+            Console.WriteLine("secend commit");
             //hello from Github
             //test to github
             Console.WriteLine("test my Github");
+
+
+
+
+
+
+
             Console.ReadKey();
         }
     }
